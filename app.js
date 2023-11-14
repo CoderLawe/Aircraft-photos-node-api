a7d64d945e9f0f9870cac93c7b81771dc54922fc
Lawe Sosah <lawesosah@Lawes-MacBook-Pro.local>
2023-11-14 16:41:24 +0200

    updated environment variables

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const port = process.env.PORT || 3001;
 const cors = require("cors");
 const app = express();
 
+const fs = require("fs");
+const { google } = require("googleapis");
+const readline = require("readline");
+
 // Allow requests from any origin
 app.use(cors());
 
 // Define an endpoint for the API
+
 app.get("/api/scrape", async (req, res) => {
   try {
     // Extract the keyword from the query parameter
@@ -45,6 +50,50 @@ app.get("/api/scrape", async (req, res) => {
       return res.status(404).json({ error: "Image source not found" });
     }
 
+    // Fetching aircraft info from Google Drive API
+
+    
+    //
+
+    // Fetch the JSON file from Google Drive
+    async function fetchGoogleDriveFile() {
+      try {
+        const token = fs.readFileSync("token.json"); // Save the access token after authentication
+        oAuth2Client.setCredentials(JSON.parse(token));
+
+        // Fetch the file content using Axios
+        const response = await axios.get(
+          `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`,
+          {
+            headers: {
+              Authorization: `Bearer ${oAuth2Client.credentials.access_token}`,
+            },
+          }
+        );
+
+        // Parse the JSON content
+        const jsonData = JSON.parse(response.data);
+
+        // Query the JSON data based on the ICAO number
+        const icaoNumberToQuery = "7c6a63"; // Replace with the ICAO number you want to search for
+        const aircraftData = jsonData.find(
+          (aircraft) => aircraft.icao === icaoNumberToQuery
+        );
+
+        if (aircraftData) {
+          console.log("Aircraft Registration:", aircraftData.registration);
+        } else {
+          console.log("Aircraft not found.");
+        }
+      } catch (error) {
+        console.error("Error fetching Google Drive file:", error);
+      }
+    }
+
+    fetchGoogleDriveFile();
+
+    // End
+
     // Return the image source in the response
     res.json({ imageSrc });
   } catch (error) {
@@ -52,6 +101,7 @@ app.get("/api/scrape", async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 });
+
 
 // Start the server
 app.listen(port, () => {
